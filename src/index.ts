0c8da387682d46a30241249b303faa2e75c3fcd2
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-03-24 23:09:53 +0300

    Clean up of comments

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,6 @@
 import type { CheckValue, AnyIndex, RemoveFromStart } from '../src/util'
 
-// DOSEN'T WORK
-// type DependantObj<T, KM extends AnyIndex> = {
-//   [Key in keyof T as RemoveFromStart<Key, 'is'>]-?: CheckKey<
-//     T[Key],
-//     KM[RemoveFromStart<Key, 'is'>],
-//     boolean,
-//     true
-//   >
-
 export type DependantObj<T, KM extends AnyIndex> = {
-  // [Key in keyof T as RemoveFromStart<Key, 'is'> ]-?: T[Key] extends true
-  //   ? KM[RemoveFromStart<Key, 'is'>]
-  //   : never
-
   [Key in keyof T as CheckValue<
     T[Key],
     true,
@@ -21,18 +8,3 @@ export type DependantObj<T, KM extends AnyIndex> = {
     never
   >]-?: CheckValue<T[Key], true, KM[RemoveFromStart<Key, 'is'>], never>
 }
-
-// interface TestKeyMap extends AnyIndex {
-//   state: 'pinia' | 'vuex'
-//   syntax: 'options' | 'composition'
-// }
-
-// const obj1 = {
-//   isState: true,
-//   isSyntax: false,
-// } as const
-
-// const obj = {
-//   state: 'pinia',
-//   // syntax: 'options',
-// } satisfies DependantObj<typeof obj1, TestKeyMap>
